608aa9857a14eb300f792bc5c852c22494dec0dd
Ahmed Baari <ahmed4baari@gmail.com>
2024-03-08 23:30:44 +0530

    Initial Setup Of Firebase

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk9dAF73p2hDN0qPpwyZENDM1ddYdKWBY",
+  authDomain: "roadcheck-870d0.firebaseapp.com",
+  projectId: "roadcheck-870d0",
+  storageBucket: "roadcheck-870d0.appspot.com",
+  messagingSenderId: "55117268796",
+  appId: "1:55117268796:web:0a89f33e26ee9fbeca8039",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
